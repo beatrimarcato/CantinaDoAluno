b801ca97b5c5bfced0dbd32b6a89cc617add55c8
Beatriz Marcato <34315837+beatrimarcato@users.noreply.github.com>
2018-09-18 19:53:42 -0300

    Layout do menu lateral
    Menu chamado no index que constrói todas as telas

---
+++ menuLateral.php
@@ -0,0 +1,76 @@
+<div class="wrapper">
+
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index.php" class="logo">
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Cantina do aluno</b></span>
+    </a>
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Navegação</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i>
+            <span>Cadastros</span>
+	    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="?pagina=alunos"><i class="fa fa-circle-o"></i> Alunos</a></li>
+            <li><a href="?pagina=funcionarios"><i class="fa fa-circle-o"></i> Funcionários</a></li>
+            <li><a href="?pagina=produtos"><i class="fa fa-circle-o"></i> Produtos</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Resultados</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="?pagina=maisVendidos"><i class="fa fa-circle-o"></i> Mais vendidos</a></li>
+            <li><a href="?pagina=menosvendidos"><i class="fa fa-circle-o"></i> Menos vendidos</a></li>
+            <li><a href="?pagina=naovendidos"><i class="fa fa-circle-o"></i> Não vendidos</a></li>
+            <li><a href="?pagina=melhoresdias"><i class="fa fa-circle-o"></i> Melhores dias</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-files-o"></i> <span>Relatórios</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/forms/[INSERIR URL DA PAG]"><i class="fa fa-circle-o"></i> Nome</a></li>
+            <li><a href="pages/forms/[INSERIR URL DA PAG]"><i class="fa fa-circle-o"></i> Nome</a></li>
+            <li><a href="pages/forms/[INSERIR URL DA PAG]"><i class="fa fa-circle-o"></i> Nome</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="pages/calendar.html">
+            <i class="fa fa-calendar"></i> <span>Calendário</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
